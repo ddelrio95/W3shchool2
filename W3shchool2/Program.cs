b56cdafbe4ac54d51312c3c472cf6c87f8dfffbd
Dario del Rio <57421573+ddelrio95@users.noreply.github.com>
2021-08-15 21:57:55 -0400

    Add project files.

---
+++ W3shchool2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace W3shchool2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Sort a string
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            Array.Sort(cars);
+            foreach (string i in cars)
+            {
+                Console.WriteLine(i);
+            }
+
+            //Sort an int
+            int[] myNumbers = {5, 1, 8, 9};
+            Array.Sort(myNumbers);
+            foreach (int i in myNumbers)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+        }
+    }
+}
